4c6b897be102a3b6f25790913507c8183c6b58ac
Vinay Grover <vinaygrover14365@gmail.com>
2024-05-06 12:04:36 +0000

    done

--- index.js
+++ index.js
@@ -1,43 +1,54 @@
-const express = require("express");
-const app = express();
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-const cors = require("cors");
-app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-app.use(express.static("public"));
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
 
-// index
+// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-// timestamp API
-app.get("/api/timestamp/:date?", (req, res) => {
-  const givenDate = req.params.date;
-  let date;
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date"
 
-  // check if no date provided
-  if (!givenDate) {
-    date = new Date();
-  } else {
-    // check if unix time:
-    //    number string multiplied by 1 gives this number, data string gives NaN
-    const checkUnix = givenDate * 1;
-    date = isNaN(checkUnix) ? new Date(givenDate) : new Date(checkUnix);
+// your first API endpoint... 
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date)
+
+  if(isInvalidDate(date)){
+    date = new Date(+req.params.date)
   }
 
-  //check if valid format
-  if (date == "Invalid Date") {
-    res.json({ error: "Invalid Date" });
-  } else {
-    const unix = date.getTime();
-    const utc = date.toUTCString();
-    res.json({ unix, utc });
+  if(isInvalidDate(date)){
+    res.json({error: "Invalid Date"})
+    return;
   }
+  
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  })
+})
+
+
+
+// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log('Your app is listening on port ' + listener.address().port);
 });
